8ee781b987d6c9d60ea659c937512e235d8772da
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-18 01:44:46 -0400

    FirstInitial

---
+++ LD/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+       Vector3 point = transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point.z = -1;
+        transform.position = point;
+
+    }
+
+
+
+}
---
+++ LD/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float walkSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * walkSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+
+
+}
