229c203bc724ad6883bf420fb4d1fb5a53b10fa2
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-18 16:44:04 -0400

    rooms2

--- LD/Assets/lavaroom1/Lava.cs
+++ LD/Assets/lavaroom1/Lava.cs
@@ -6,6 +6,9 @@ public class Lava : MonoBehaviour
 {
     public GameObject MGMT;
     // Start is called before the first frame update
+    public float speed = 1f;
+    public bool closeDirUpLeft = true;
+    public bool vert = true;
     void Start()
     {
         
@@ -14,7 +17,30 @@ public class Lava : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (vert)
+        {
+            if (closeDirUpLeft)
+            {
+                transform.Translate(Vector2.up * 0.07f * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector2.down * 0.07f * speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (closeDirUpLeft) {
+                transform.Translate(Vector2.left * 0.07f * speed * Time.deltaTime) ;
+        }
+            else
+        {
+            transform.Translate(Vector2.right * 0.07f * speed * Time.deltaTime);
+        }
+
+    }
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
