c6457ac2c22544d0aec497400459c2f831dbaf37
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-18 12:20:04 -0400

    typewriter effect

---
+++ LD/Assets/textWriter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class textWriter : MonoBehaviour
+{
+    public float gap = 0.2f;
+    public Text message;
+    public string current = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void write(string t)
+    {
+        StartCoroutine(TypeWrite(t, message));
+    }
+
+    private IEnumerator TypeWrite(string text, Text body)
+    {
+        foreach(char character in text.ToCharArray())
+        {
+            current += character;
+            body.text = current;
+            yield return new WaitForSeconds(gap);
+
+        }
+
+
+    }
+
+}
