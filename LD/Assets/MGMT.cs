403d5c0c82e4ce74f082f497e2cd10c6627eaff4
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-18 12:42:30 -0400

    portals

---
+++ LD/Assets/MGMT.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGMT : MonoBehaviour
+{
+    public GameObject messageMGMT;
+    public int INDEX = -1;
+    public string[] dialog = new string[] { "YOU WILL BE TESTED", "MAKE IT TO THE OTHER SIDE" };
+    public bool writing = false;
+    // Start is called before the first frame update
+  
+    void NextMessage()
+    {
+        INDEX++;
+        Debug.Log(dialog[INDEX]);
+        messageMGMT.transform.SendMessage("write", dialog[INDEX]);
+        writing = true;
+    }
+
+    void Start()
+    {
+        NextMessage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void done()
+    {
+        writing = false;
+    }
+}
--- LD/Assets/textWriter.cs
+++ LD/Assets/textWriter.cs
@@ -8,6 +8,8 @@ public class textWriter : MonoBehaviour
     public float gap = 0.2f;
     public Text message;
     public string current = "";
+    public GameObject gameMGMT;
+    int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +26,22 @@ public class textWriter : MonoBehaviour
     void write(string t)
     {
         StartCoroutine(TypeWrite(t, message));
+        counter = 0;
     }
 
     private IEnumerator TypeWrite(string text, Text body)
     {
+        
         foreach(char character in text.ToCharArray())
         {
+            if(counter == text.Length - 1)
+            {
+                gameMGMT.transform.SendMessage("done");
+            }
             current += character;
             body.text = current;
             yield return new WaitForSeconds(gap);
+            counter++;
 
         }
 
