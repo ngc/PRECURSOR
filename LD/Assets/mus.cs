2c3592fd14d4223bf7ba8a6a5fe586a4be3678f6
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-20 00:57:30 -0400

    FinalBuild

---
+++ LD/Assets/mus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mus : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
